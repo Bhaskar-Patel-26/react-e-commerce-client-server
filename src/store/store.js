938d133653352190741261bcbc9cf920d0b0ad5b
Bhaskar Patel <120586781+Bhaskar-Patel-26@users.noreply.github.com>
2025-02-05 21:52:11 +0530

    Product API usign RTK query is completed

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../features/cart/cartSlice";
 import authAPI from "../features/auth/authAPI";
 import authReducer from "../features/auth/authSlice";
+import productsAPI from "../features/products/productsAPI";
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
     [authAPI.reducerPath]: authAPI.reducer,
     auth: authReducer,
+    [productsAPI.reducer]: productsAPI.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authAPI.middleware),
+    getDefaultMiddleware().concat(authAPI.middleware, productsAPI.middleware),
 });
